192b7c286bcf004503807bd9c02541fec59d2281
Rachita5873 <76832946+Rachita5873@users.noreply.github.com>
2023-07-16 10:48:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+secondHighest(arr){
+	let num = arr.sort();
+	if(num.length===0 || num.length==1 || num[0]==num[num.length-1]){
+		return "-Infinity"
+	}
+		
+	else{
+		return num[num.length-2];
+	}
+}
